383a73753b5e87a75f8a5978dc5b8b6898d35f7a
Andrei Arkhipov <iamckuff@gmail.com>
2026-04-17 10:51:18 +0300

    delete unused scripts

--- check_db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('src/data/magnit.db')
-cursor = conn.cursor()
-cursor.execute("SELECT sql FROM sqlite_master WHERE type='table' AND name='categories'")
-result = cursor.fetchone()
-print(result[0] if result else "Table not found")
-conn.close()
